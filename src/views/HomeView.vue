9c6f2e007d90e313c796c2a7edecf5eec6c5a3db
divinejoshua <divineuche399@gmail.com>
2023-03-11 18:15:46 +0000

    Made changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -138,9 +138,9 @@
       if(isLoading.value==true) { return } //Return if the form is loading
       if (!contextData.value || !question.value) { return }  // Return if there is no question or contextData 
 
-      this.isErrorRequest = false //Set error to false 
-
       this.messagesData = []
+
+      this.isErrorRequest = false //Set error to false 
 
       // Add the contextData and question to the messagesData object
       this.messagesData.push({role: "assistant", content: this.contextData})
