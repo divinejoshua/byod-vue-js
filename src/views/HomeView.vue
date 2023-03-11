5b05edbb15d743cbc060816b5b108efcf2731592
divinejoshua <divineuche399@gmail.com>
2023-03-11 13:47:53 +0000

    Added messagesData list of arrays

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,10 +18,13 @@
         <h5>Information re-defined</h5>
 
         <br>
-        <p class="text-muted text-sm">BYOD (Bring Your Own Data) a powerful tool that uses artificial intelligence to help you fine-tune data from your word documents. With Boyd, you can easily extract and analyze data from your documents, such as names, dates, and numbers, and then use our intuitive interface to make adjustments and corrections as needed. 
+        <!-- <p class="text-muted text-sm">BYOD (Bring Your Own Data) a powerful tool that uses artificial intelligence to help you fine-tune data from your word documents. With Boyd, you can easily extract and analyze data from your documents, such as names, dates, and numbers, and then use our intuitive interface to make adjustments and corrections as needed. 
             <br><br>
             Our AI algorithms are designed to learn from your input, so the more you use the app, the smarter it becomes. Whether you're a business professional, researcher, or student, Boyd is the perfect tool for anyone who needs to work with large amounts of data in their word documents.
-        </p>
+        </p> -->
+
+        messagesData:
+        {{messagesData}}
 
       <!-- Data context form  -->
         <form>
@@ -51,24 +54,74 @@
 <script setup>
 /* eslint-disable */
 import { ref, onMounted } from 'vue';
+import { Configuration, OpenAIApi } from "openai";    //OpenAI 
 
 //state
-const contextData = ref("");
-const question = ref("");
-const answer = ref("");
+let contextData = ref("");
+let question = ref("");
+let answer = ref("");
+let isLoading = ref(false);
+let messagesData = ref([]);
+
+
+// Create configuration object
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// create OpenAI configuration object
+const openai = new OpenAIApi(configuration);
+
+
 
 
 
 // =====methods
 
-// This method submits the form
-function submitForm(){
+    // This method submits the form
+    async function submitForm(){
 
-    // Return if there is no question or contextData 
-  if (!contextData.value || !question.value) { return }
+      this.messagesData = []
+
+      this.messagesData.push({role: "assistant", content: contextData})
+      this.messagesData.push({role: "user", content: "first"})
+
+      if(isLoading.value==true) { return } //Return if the form is loading
+      if (!contextData.value || !question.value) { return }  // Return if there is no question or contextData 
 
 
-}
+      this.isLoading = true
+
+      // Send the request 
+      try {
+
+        let response = await openai.createChatCompletion({ 
+          model: "gpt-3.5-turbo",
+          temperature: 0,
+          max_tokens: 2000,
+          top_p: 0,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+          stop: ["{}"],
+          messages: newMessages,
+        });
+
+        //set the result to answer
+        this.answer = response.data.choices[0].message.content
+
+        }
+
+        catch{
+
+        }
+
+        finally{
+
+          console.log(isLoading.value)
+        }
+
+
+    }
 
 
 
